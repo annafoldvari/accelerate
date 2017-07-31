92107f14b46bbba23f03ae39fca8c1c28e399014
anna_f <annafoldvari@gmx.co.uk>
2017-07-31 16:47:40 +0100

    Custom field added to case studies.

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,9 +12,46 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			    <?php the_title('<h1>', '</h1>'); ?>
+			<?php while ( have_posts() ) : the_post();
+			$size = "full";
+			$services = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image = get_field('image');
+			$image2 = get_field('image_2');
+			$image3 = get_field('image_3');
+			?>
+			    
+			<aside class="case-study-sidebar">
+
+				<h2><?php the_title(); ?></h2>
+				<h5><?php echo $services; ?></h5>
+				<h6>Client: <?php echo $client; ?></h6>
+
 				<?php the_content(); ?>
+				
+				<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+
+			</aside>
+
+			<div class="case-study-images">
+			
+				<?php if($image) { ?>
+				     <img src="<?php echo wp_get_attachment_image( $image, $size ); ?>" />
+				
+				<?php } ?> 
+
+				<?php if($image2) { ?>
+				     <img src="<?php echo wp_get_attachment_image( $image2, $size ); ?>" />
+				
+				<?php } ?> 
+				
+				<?php if($image3) { ?>
+				     <img src="<?php echo wp_get_attachment_image( $image3, $size ); ?>" />
+				
+				<?php } ?> 
+				
+			</div>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
